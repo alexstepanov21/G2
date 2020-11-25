2d1ebf22f76adb6e498cf1ece2aef0afeb512fe1
alexstepanov21 <74997529+alexstepanov21@users.noreply.github.com>
2020-11-25 07:53:07 +0300

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c, x1, x2, d As Integer
+        a = InputBox("a=")
+        b = InputBox("b=")
+        c = InputBox("c=")
+        d = b ^ 2 - 4 * a * c
+        MsgBox(d)
+        If d > 0 Then
+            x1 = (-b + d ^ 0.5) / 2 * a
+            x2 = (-b - d ^ 0.5) / 2 * a
+            MsgBox(x1)
+            MsgBox(x2)
+        ElseIf d = 0 Then
+            x1 = -b / 2 * a
+            MsgBox(x1)
+        Else
+            MsgBox("<0")
+        End If
+    End Sub
+End Class
